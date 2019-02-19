ecc471312c4e522fc940fd64194e9b37314e271d
laurel-thomson <laurel228@ksu.edu>
2019-02-19 17:01:55 -0600

    Moving JoinSessionActivity into the UI folder and adding it to the manifest file

--- app/src/main/java/club/dev/mobile/ksu/takeanumber/UI/JoinSessionActivity.java
+++ app/src/main/java/club/dev/mobile/ksu/takeanumber/UI/JoinSessionActivity.java
@@ -1,9 +1,11 @@
-package club.dev.mobile.ksu.takeanumber;
+package club.dev.mobile.ksu.takeanumber.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import club.dev.mobile.ksu.takeanumber.R;
 
 public class JoinSessionActivity extends AppCompatActivity {
 
