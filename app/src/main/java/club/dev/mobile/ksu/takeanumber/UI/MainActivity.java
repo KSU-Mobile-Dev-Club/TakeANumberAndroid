4f30cfd9f306137b01d2936ced81070c83640f95
Trent <tkempker@ksu.edu>
2019-04-10 20:52:56 -0500

    Launch join session activity from main activity

--- app/src/main/java/club/dev/mobile/ksu/takeanumber/UI/MainActivity.java
+++ app/src/main/java/club/dev/mobile/ksu/takeanumber/UI/MainActivity.java
@@ -2,9 +2,12 @@ package club.dev.mobile.ksu.takeanumber.UI;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -33,12 +36,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onChanged(@Nullable List<HelpSession> helpSessions) {
                 adapter.setHelpSessionsList(helpSessions);
+                sessionsList = helpSessions;
             }
         });
 
-        sessionsList.add(new HelpSession("Test"));
 
         listView = findViewById(R.id.HelpSessionListView);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, JoinSessionActivity.class);
+                intent.putExtra("sessionKey", sessionsList.get(position).getFirebaseKey());
+                MainActivity.this.startActivity(intent);
+            }
+        });
 
         adapter = new HelpSessionAdapter(this, R.layout.list_item); //Adapter(this, R.layout.list_item, sessionsList)
 
